9258d57656a4754fd11633601a903502a85dec4d
ciprian_andrei.hoza <ciprian_andrei.hoza@stud.acs.upb.ro>
2026-03-13 12:47:30 +0200

    added the insert cascade test

--- src/test/java/Structures/TerminalBufferTest.java
+++ src/test/java/Structures/TerminalBufferTest.java
@@ -41,6 +41,38 @@ public class TerminalBufferTest {
         assertTrue((fontB & Cell.UNDERLINE) != 0, "Underline bit should be set");
         assertTrue((fontB & Cell.ITALIC) != 0, "Italic bit should be set");
         assertFalse((fontB & Cell.BOLD) != 0, "Bold bit should not be set");
-
     }
+
+    @Test
+    @DisplayName("Insert at start or full screen should cascade characters down multiple lines")
+    void testInsertCascade()
+    {
+        //Filling the first 3 lines with A, B, C
+        buffer.fillLine(0, 'A');
+        buffer.fillLine(1, 'B');
+        buffer.fillLine(2, 'C');
+
+        //Resetting the cursor and inserting the character 'X' at the start
+        buffer.cursor.setX(0);
+        buffer.cursor.setY(0);
+        buffer.insert("X");
+
+        //The first character should be 'X' and the second 'A'
+        assertEquals('X', (char)buffer.getCharacter(0, 0));
+        assertEquals('A', (char)buffer.getCharacter(1, 0));
+
+        //On the second line, the first character should be 'A' and the rest 'B'
+        assertEquals('A', (char)buffer.getCharacter(1, 0));
+        assertEquals('B', (char)buffer.getCharacter(1, 1));
+
+        //On the third line, the first character should be 'B' and the rest 'C'
+        assertEquals('B', (char)buffer.getCharacter(0, 2));
+        assertEquals('C', (char)buffer.getCharacter(1, 2));
+
+        //On the forth line, the only character should be 'C'
+        assertEquals('C', (char)buffer.getCharacter(0, 3));
+        assertEquals(' ', (char)buffer.getCharacter(1, 3));
+    }
+
+
 }
